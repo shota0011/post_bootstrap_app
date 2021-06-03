5596e00a46388b0fdd5fb343a40a36389a713772
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-06-03 22:00:17 +0900

    詳細表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def new
     @post = Post.new
   end
@@ -8,9 +9,13 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
-    params.require(:post).premit(:title, :content)
+    params.require(:post).permit(:title, :content)
   end
 end
