b10f366dbf89c4f53ab54bc35f12efe3a952397c
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-06-06 15:48:50 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,16 @@ class PostsController < ApplicationController
       redirect_to root_path
   end
 
+  def edit
+  @post = Post.find(params[:id])
+  end
+
+  def update
+  post = Post.find(params[:id])
+  post.update!(post_params)
+  redirect_to posts_path
+  end
+
   private
 
   def post_params
